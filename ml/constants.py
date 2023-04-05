dde7f21f08dfabf50d353a0ed7eb53a751b03298
Felipe Ferraz <felipe.ferraz@engenharia.ufjf.br>
2023-04-05 00:30:53 -0300

    ✨ Add constants file

---
+++ ml/constants.py
@@ -0,0 +1,14 @@
+CATEGORICAL_FEATURES = [
+    'workclass',
+    'education',
+    'marital-status',
+    'occupation',
+    'relationship',
+    'race',
+    'sex',
+    'native-country'
+]
+
+LABEL = 'salary'
+
+DATA_FILE = 'data/census.csv'
